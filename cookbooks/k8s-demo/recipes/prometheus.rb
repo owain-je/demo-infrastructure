c301cabea0313d63fe6e47e0ebe9a0644a1d8bec
owain perry <owain.perry@just-eat.com>
2017-07-25 16:16:34 +0000

    ingress rules from prometheus

--- cookbooks/k8s-demo/recipes/prometheus.rb
+++ cookbooks/k8s-demo/recipes/prometheus.rb
@@ -13,14 +13,14 @@ namespace = "infra"
 #
 # helm upgrade prometheus-op ./kube-prometheus --install --namespace=infra -f k8s-infrastructure/cookbooks/k8s-venus/files/default/values/infra/prometheus-op.yaml --debug
 
-helm_package "je-charts/kube-prometheus" do 
-	values_files File.join(node[cookbook_name]['path'],namespace,"prometheus-op.yaml")
-	release_name "prometheus-op"
-	version "0.2.0"
-	kube_context node[cookbook_name]['context']
-	namespace namespace
-	action [:upgrade]
-end
+#helm_package "je-charts/kube-prometheus" do 
+#	values_files File.join(node[cookbook_name]['path'],namespace,"prometheus-op.yaml")
+#	release_name "prometheus-op"
+#	version "0.2.0"
+#	kube_context node[cookbook_name]['context']
+#	namespace namespace
+#	action [:upgrade]
+#end
  
 helm_package "je-charts/prometheus-servicemonitor" do 
 	values_files File.join(node[cookbook_name]['path'],namespace,"prometheus-servicemonitor.yaml")
